1db5d243ac90bfdeb3d2a9adbb4af2c8e4a4bb94
Igor Yalovoy <to@ylv.io>
2022-06-24 16:52:38 +0300

    Pool swap method

--- src/Pool.sol
+++ src/Pool.sol
@@ -2,10 +2,14 @@
 
 pragma solidity ^0.8.13;
 
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
 contract Pool {
 
 
-    function swap(uint256 amount, IERC20 token) external {
+    function swap(IERC20 _from, uint256 _amount, IERC20 _to) external {
+        _from.transferFrom(msg.sender, address(this), _amount);
+        _to.transfer(msg.sender, _amount);
     }
 
 }
