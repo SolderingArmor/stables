8008ece2663e0b8f8b85b57cc864055ff3b1366b
Igor Yalovoy <to@ylv.io>
2022-06-24 12:53:44 +0300

    Add pool

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -4,9 +4,25 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 
 contract ContractTest is Test {
-    function setUp() public {}
+    uint256 foobar;
+
+    function setUp() public {
+        foobar = 42;
+    }
+
+    function testFoobarSecond() public {
+        assertEq(foobar, 42);
+    }
+
+    function testFoobar() public {
+        assertEq(foobar, 42);
+    }
 
     function testExample() public {
         assertTrue(true);
+    }
+
+    function testFuz(uint8 amount) external {
+        assertEq(amount, amount);
     }
 }
---
+++ test/Pool.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+contract PoolTest is Test {
+
+    function setUp() public {
+    }
+
+    function testInit() public {}
+
+}
