90516dff93fe7b8c023009372010b58a0681faaf
Igor Yalovoy <to@ylv.io>
2022-06-24 17:26:12 +0300

    Add TestERC20

--- test/Pool.t.sol
+++ test/Pool.t.sol
@@ -3,8 +3,64 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
-contract PoolTest is Test {
-    function setUp() public {}
+import { TestERC20 } from "../../../contracts/test/TestERC20.sol";
 
-    function testInit() public {}
+
+contract PoolTest is Test {
+    /**
+     * @dev Deploy test token contracts
+     */
+    function _deployTestTokenContracts() internal {
+        token1 = new TestERC20();
+        token2 = new TestERC20();
+        token3 = new TestERC20();
+        test721_1 = new TestERC721();
+        test721_2 = new TestERC721();
+        test721_3 = new TestERC721();
+        test1155_1 = new TestERC1155();
+        test1155_2 = new TestERC1155();
+        test1155_3 = new TestERC1155();
+        preapproved721 = new PreapprovedERC721(preapprovals);
+
+        vm.label(address(token1), "token1");
+        vm.label(address(test721_1), "test721_1");
+        vm.label(address(test1155_1), "test1155_1");
+        vm.label(address(preapproved721), "preapproved721");
+
+        emit log("Deployed test token contracts");
+    }
+
+    /**
+     * @dev Allocate amount of each token to _to
+     */
+    function allocateTokensAndApprovals(address _to, uint128 _amount) internal {
+        vm.deal(_to, _amount);
+        for (uint256 i = 0; i < erc20s.length; ++i) {
+            erc20s[i].mint(_to, _amount);
+        }
+        emit log_named_address("Allocated tokens to", _to);
+        _setApprovals(_to);
+    }
+
+    function setUp() public {
+        super.setUp();
+
+        vm.label(alice, "alice");
+        vm.label(bob, "bob");
+        vm.label(cal, "cal");
+        vm.label(address(this), "testContract");
+
+        _deployTestTokenContracts();
+        erc20s = [token1, token2, token3];
+        erc721s = [test721_1, test721_2, test721_3];
+        erc1155s = [test1155_1, test1155_2, test1155_3];
+
+        // allocate funds and tokens to test addresses
+        allocateTokensAndApprovals(address(this), uint128(MAX_INT));
+        allocateTokensAndApprovals(alice, uint128(MAX_INT));
+        allocateTokensAndApprovals(bob, uint128(MAX_INT));
+        allocateTokensAndApprovals(cal, uint128(MAX_INT));
+    }
+
+    function testSwap() public {}
 }
